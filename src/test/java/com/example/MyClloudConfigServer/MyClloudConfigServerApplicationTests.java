cb5cb9d345ea20d2873bd1a42602b8944ef7bb09
srinivas samala <srinu1657@gmail.com>
2023-10-09 02:15:01 -0500

    intial commited

---
+++ src/test/java/com/example/MyClloudConfigServer/MyClloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyClloudConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyClloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
